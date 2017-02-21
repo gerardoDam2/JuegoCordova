708eb9b3b2129b2a972945893432636bb7108319
gerardoDam2 <sccodeow@gmail.com>
2017-02-21 23:28:11 +0000

    game over
    pendiende de implementar persistencia de las vidas.

--- js/index.js
+++ js/index.js
@@ -32,11 +32,13 @@ var paddleWidth = 75;
 var paddleHeight = 10;
 var paddleX = (canvas.width - paddleWidth) / 2;
 
-var lifes= 3;
+var lifes = 3;
+var isGameOver = false;
+var esperar=0;
 
 function initApp() {
     acelerometro = navigator.accelerometer.watchAcceleration(onAcelerometroCall, failure, {
-        frequency: 10
+        frequency: 1
     });
 }
 
@@ -56,55 +58,73 @@ function drawPaddle() {
     ctx.closePath();
 }
 
-function drawLifes(){
-ctx.font = canvas.width+"px Arial";
-ctx.textAlign="center";
-ctx.fillText(lifes,canvas.width/2,canvas.height/2);
+function drawLifes() {
+  //  ctx.font = canvas.width + "px Arial";
+    ctx.font = "200px Arial";
+    ctx.textAlign = "center";
+    ctx.textBaseline="midle"
+    ctx.fillText(lifes, canvas.width / 2, canvas.height / 2);
 }
 
 
 
-function gameOver(){
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.font = "100px Arial";
-  ctx.textAlign="center";
-  ctx.fillText("Game Over",canvas.width/2,30);
+function gameOver() {
+    isGameOver=true;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.font = "40px Arial";
+    ctx.textAlign = "center";
+    ctx.textBaseline="midle"
+    ctx.fillText("GAME OVER", canvas.width / 2, canvas.height / 2);
 
 }
 
 
 function onAcelerometroCall(acelerometroValue) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawBall();
-  drawPaddle();
-  drawLifes();
-    if ((y + dy) < ballRadius) {
-        dy = -dy;
-    } else if ((y + dy) > canvas.height - ballRadius) {
-        if (x > paddleX && x < paddleX + paddleWidth) {
+    if (isGameOver){
+      esperar++;
+      if (esperar==500) {
+        isGameOver=false;
+      }
+    }
+
+     else{
+
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawLifes();
+        drawBall();
+        drawPaddle();
+
+        if ((y + dy) < ballRadius) {
             dy = -dy;
-        } else {
-          lifes-=1;
-          x=1+ballRadius;
-          y=1+ballRadius;
-          if (lifes==0) {
-            lifes=3;
-            gameOver();
+        } else if ((y + dy) > canvas.height - ballRadius) {
+            if (x > paddleX && x < paddleX + paddleWidth) {
+                dy = -dy;
+            } else {
+                lifes -= 1;
+                x = 1 + ballRadius;
+                y = 1 + ballRadius;
+                if (lifes == 0) {
+                    lifes = 3;
+                    esperar=0;
+                    gameOver();
 
-          }
+                }
 
+            }
         }
+        if (((x + dx) > canvas.width - ballRadius) || ((x + dx) < ballRadius)) {
+            dx = -dx;
+        }
+        if (acelerometroValue.x > 0 && paddleX > 0) {
+            paddleX += -1 * (acelerometroValue.x * 1.8);
+        } else if (acelerometroValue.x < 0 && paddleX < canvas.width - paddleWidth) {
+            paddleX -= (acelerometroValue.x * 1.8);
+        }
+        x += dx;
+        y += dy;
     }
-    if (((x + dx) > canvas.width - ballRadius) || ((x + dx) < ballRadius)) {
-        dx = -dx;
-    }
-    if (acelerometroValue.x > 0 && paddleX > 0) {
-        paddleX += -1 * (acelerometroValue.x * 1.8);
-    } else if (acelerometroValue.x < 0 && paddleX < canvas.width - paddleWidth) {
-        paddleX -= (acelerometroValue.x * 1.8);
-    }
-    x += dx;
-    y += dy;
 }
 
 function failure() {
